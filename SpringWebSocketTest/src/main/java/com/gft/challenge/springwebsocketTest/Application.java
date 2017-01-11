8ada3e638b1202fd2f95bb9845c6525d49780e15
armkGFT <artur.mucik@gft.com>
2017-01-11 13:42:32 +0100

    Renaming main class and making it work.

---
+++ SpringWebSocketTest/src/main/java/com/gft/challenge/springwebsocketTest/Application.java
@@ -0,0 +1,16 @@
+package com.gft.challenge.springwebsocketTest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by armk on 2017-01-11.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- SpringWebSocketTest/src/main/java/com/gft/challenge/springwebsocketTest/Main.java
+++
@@ -1,6 +0,0 @@
-package com.gft.challenge.springwebsocketTest;
-/**
- * Created by armk on 2017-01-11.
- */
-public class Main {
-}
